b45ea90a06b97e658d8cc01821d0770109390024
Adam Washington <adam.washington@stfc.ac.uk>
2020-06-30 17:38:23 +0100

    Allow parsing many files

--- cpp_parser.cpp
+++ cpp_parser.cpp
@@ -8,17 +8,21 @@
 using namespace std;
 using namespace antlr4;
 int main(int argc, const char* argv[]) {
-  std::ifstream stream;
-  stream.open(argv[1]);
+  for (int i = 1; i < argc; ++i) {
+    std::ifstream stream;
+    std::cout << "Loading " << argv[i] << std::endl;
+    stream.open(argv[i]);
 
-  ANTLRInputStream input(stream);
-  DissolveLexer lexer(&input);
-  CommonTokenStream tokens(&lexer);
-  DissolveParser parser(&tokens);
-  DissolveParser::ProgramContext* tree = parser.program();
-  // std::cout << tree->toStringTree(&parser) << std::endl;
+    ANTLRInputStream input(stream);
+    DissolveLexer lexer(&input);
+    CommonTokenStream tokens(&lexer);
+    DissolveParser parser(&tokens);
+    DissolveParser::ProgramContext* tree = parser.program();
+    // std::cout << tree->toStringTree(&parser) << std::endl;
 
-  MyVisitor visitor;
-  visitor.visit(tree);
+    MyVisitor visitor;
+    visitor.visit(tree);
+    std::cout << "********************************************************************************" << std::endl;
+  }
   return 0;
 }
