df72d4dab6a41cd10e95436adde666b7750d59d9
Adam Washington <adam.washington@stfc.ac.uk>
2020-07-07 17:09:21 +0100

    Add the range to the Pair Potential

---
+++ Visitor-pairPotential.cpp
@@ -0,0 +1,43 @@
+#include "Visitor.h"
+#include "types.h"
+#include <algorithm>
+#include <iostream>
+#include <memory>
+#include <tuple>
+#include <vector>
+
+antlrcpp::Any
+MyVisitor::visitPairPotential(DissolveParser::PairPotentialContext *context) {
+  std::vector<PairPotentialParameters> params;
+  double range = 0;
+  for (auto &par : context->pairPotentialsParameters()) {
+    params.push_back(visitPairPotentialsParameters(par));
+  }
+  if (!context->pairPotentialsRange().empty()) {
+    range = visit(context->pairPotentialsRange(0)->num());
+  }
+
+  return PairPotential(params, range);
+};
+
+antlrcpp::Any MyVisitor::visitPairPotentialsParameters(
+    DissolveParser::PairPotentialsParametersContext *context) {
+  auto result =
+      PairPotentialParameters(visit(context->name), visit(context->element),
+			      visit(context->strength), visit(context->pp()));
+  return result;
+}
+
+antlrcpp::Any MyVisitor::visitPp(DissolveParser::PpContext *context) {
+  std::vector<double> params;
+  if (context->lj()) {
+    for (auto p : context->lj()->num())
+      params.push_back(visit(p));
+    return PP(PP::LJ, params);
+  }
+  if (context->ljGeometric()) {
+    for (auto p : context->ljGeometric()->num())
+      params.push_back(visit(p));
+    return PP(PP::LJGeometric, params);
+  }
+};
