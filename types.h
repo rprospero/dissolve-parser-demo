0ffcf5435e4f9180e35bcfc71415f4945c6592a5
Adam Washington <adam.washington@stfc.ac.uk>
2020-07-08 11:13:37 +0100

    clang-format types.h

--- types.h
+++ types.h
@@ -8,8 +8,8 @@
 #pragma once
 
 #include <iostream>
-#include <string>
 #include <optional>
+#include <string>
 #include <tuple>
 #include <variant>
 #include <vector>
@@ -17,9 +17,8 @@
 // This class defines a three-vector of doubles
 struct Vec3 {
   double x, y, z;
-  friend std::ostream& operator<<(std::ostream& out, const Vec3& vec);
+  friend std::ostream &operator<<(std::ostream &out, const Vec3 &vec);
 };
-
 
 /*
   We use a variant to represent the different types of bond definition
@@ -29,19 +28,19 @@ struct Vec3 {
  */
 struct Harmonic {
   double i, j;
-  friend std::ostream& operator<<(std::ostream& out, const Harmonic& bond);
+  friend std::ostream &operator<<(std::ostream &out, const Harmonic &bond);
 };
 
 struct Cos3 {
   Vec3 vec;
-  friend std::ostream& operator<<(std::ostream& out, const Cos3& bond);
+  friend std::ostream &operator<<(std::ostream &out, const Cos3 &bond);
 };
 
 using BondKind = std::variant<Harmonic, Cos3>;
 
 // Since BondKind isn't a real class, we need to declare the <<
 // operator separately.
-std::ostream& operator<<(std::ostream& out, const BondKind& bond);
+std::ostream &operator<<(std::ostream &out, const BondKind &bond);
 
 // The class for a single atom in Dissolve
 struct Atom {
@@ -49,35 +48,37 @@ struct Atom {
   Vec3 position;
   std::string element, type;
   std::optional<double> charge;
-  friend std::ostream& operator<<(std::ostream& out, const Atom& atom);
+  friend std::ostream &operator<<(std::ostream &out, const Atom &atom);
 };
 
 // The class for the bond between two atoms
 struct Bond {
   int i, j;
   BondKind tag;
-  friend std::ostream& operator<<(std::ostream& out, const Bond& bond);
+  friend std::ostream &operator<<(std::ostream &out, const Bond &bond);
 };
 
 // The class for the angle between three atoms
 struct Angle {
   int a, b, c;
   BondKind tag;
-  friend std::ostream& operator<<(std::ostream& out, const Angle& angle);
+  friend std::ostream &operator<<(std::ostream &out, const Angle &angle);
 };
 
 // The class for the torsion across four atoms.
 struct Torsion {
   int a, b, c, d;
   BondKind tag;
-  friend std::ostream& operator<<(std::ostream& out, const Torsion& torsion);
+  friend std::ostream &operator<<(std::ostream &out, const Torsion &torsion);
 };
 
-// A collectiong of Isotopologues.  We may need to parse and define these a bit better
+// A collectiong of Isotopologues.  We may need to parse and define these a bit
+// better
 struct Isotopologue {
   std::string name;
   std::vector<std::string> kind;
-  friend std::ostream& operator<<(std::ostream& out, const Isotopologue& isotopologue);
+  friend std::ostream &operator<<(std::ostream &out,
+				  const Isotopologue &isotopologue);
 };
 
 // The location of a site within a species.  We may want to split this
@@ -88,19 +89,26 @@ struct Site {
   bool massWeighted;
   int xaxis;
   int yaxis;
-  friend std::ostream& operator<<(std::ostream& out, const Site& site);
+  friend std::ostream &operator<<(std::ostream &out, const Site &site);
 };
 
 // Which forcefield to use for a species
 struct Forcefield {
   std::string name;
-  friend std::ostream& operator<<(std::ostream& out, const Forcefield& forcefield);
+  friend std::ostream &operator<<(std::ostream &out,
+				  const Forcefield &forcefield);
 };
 
 // A single species within the simulation
 class Species {
 public:
-  Species(std::string name, std::vector<Atom> atoms, std::vector<Bond> bonds, std::vector<Angle> angles, std::vector<Torsion> torsions, std::vector<Isotopologue> isos, std::vector<Site> sites, std::vector<Forcefield> forcefields) : name_(name), atoms_(atoms), bonds_(bonds), angles_(angles), torsions_(torsions), isotopologues_(isos), sites_(sites), forcefields_(forcefields) {}
+  Species(std::string name, std::vector<Atom> atoms, std::vector<Bond> bonds,
+	  std::vector<Angle> angles, std::vector<Torsion> torsions,
+	  std::vector<Isotopologue> isos, std::vector<Site> sites,
+	  std::vector<Forcefield> forcefields)
+      : name_(name), atoms_(atoms), bonds_(bonds), angles_(angles),
+	torsions_(torsions), isotopologues_(isos), sites_(sites),
+	forcefields_(forcefields) {}
   std::string name_;
   std::vector<Atom> atoms_;
   std::vector<Bond> bonds_;
@@ -109,25 +117,29 @@ public:
   std::vector<Isotopologue> isotopologues_;
   std::vector<Site> sites_;
   std::vector<Forcefield> forcefields_;
-  friend std::ostream& operator<<(std::ostream& out, const Species& s);
+  friend std::ostream &operator<<(std::ostream &out, const Species &s);
 };
 
 class PP {
 public:
-  enum PPType {LJ, LJGeometric};
+  enum PPType { LJ, LJGeometric };
   PP(PPType type, std::vector<double> params) : params_(params), type_(type) {}
-  friend std::ostream& operator<<(std::ostream& out, const PP& p);
+  friend std::ostream &operator<<(std::ostream &out, const PP &p);
+
 private:
   PPType type_;
   std::vector<double> params_;
 };
 
-
 // The Pair Potential terms
 class PairPotentialParameters {
 public:
-  PairPotentialParameters(std::string name, std::string element, double strength, PP type) : name_(name), element_(element), strength_(strength), type_(type) {};
-  friend std::ostream& operator<<(std::ostream& out, const PairPotentialParameters& p);
+  PairPotentialParameters(std::string name, std::string element,
+			  double strength, PP type)
+      : name_(name), element_(element), strength_(strength), type_(type){};
+  friend std::ostream &operator<<(std::ostream &out,
+				  const PairPotentialParameters &p);
+
 private:
   std::string name_, element_;
   double strength_;
